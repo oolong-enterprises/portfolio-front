d6a1ec78f028c2d27bb10ee686b5f326efe1ea4a
oolon <oolongenterprises@gmail.com>
2025-10-28 15:52:56 -0400

    login Button

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,27 +1,26 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
 const LoginButton = () => {
   const { isLoggedIn, logout } = useAuth();
   const navigate = useNavigate();
 
-  const handleClick = (e) => {
-    if (!isLoggedIn) {
-      navigate("/login");
-    } else {
-      e.preventDefault();
+  const handleClick = () => {
+    if (isLoggedIn) {
       logout();
+    } else {
+      navigate("/login");
     }
   };
 
   return (
     <div className="ml-[10vw]">
-      <Link
-        to={isLoggedIn ? "/login" : "/"}
+      <button
         onClick={handleClick}
-        className="bg-blue-500 text-white py-3 px-6 font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] rounded-lg">
-        {isLoggedIn ? "Login" : "Log Out"}
-      </Link>
+        className="bg-blue-500 text-white py-3 px-6 font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] rounded-lg"
+      >
+        {isLoggedIn ? "Log Out" : "Login"}
+      </button>
     </div>
   );
 };
